8c33e640bf0e21b8288f6dd6f18114e542f59997
dev-OwaisKhan <dev.owaiskhan@gmail.com>
2022-09-09 22:28:03 +0530

    recycler view added

--- app/src/main/java/RecyclerViewAdapter/RecyclerViewAdapter.java
+++ app/src/main/java/RecyclerViewAdapter/RecyclerViewAdapter.java
@@ -40,7 +40,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 1;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
